a33404f9f7410cc5af9c47b663d6aed8032358a4
vahid35330 <mohammadvahid35330@gmail.com>
2022-06-04 14:05:38 +0430

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'productName',
+    'category',
+    'date',
+    'freshness',
+    'price',
+    'comment',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog
+      .open(DialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getAllProducts();
+        }
+      });
+  }
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: () => {
+        alert('Error while fetching the Records!');
+      },
+    });
+  }
+  editProduct(row: any) {
+    this.dialog
+      .open(DialogComponent, { width: '30%', data: row })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getAllProducts();
+        }
+      });
+  }
+  deleteProduct(id: number) {
+    this.api.deleteProduct(id).subscribe({
+      next: (res) => {
+        alert('Deleted successfully!');
+        this.getAllProducts();
+      },
+      error: () => {
+        alert('Error while deleting!');
+      },
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
